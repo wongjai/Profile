17c0e1ea60f712291d1766c8c7fd3b0cbc709b50
wongjai <40750736+wongjai@users.noreply.github.com>
2020-08-14 17:45:27 +0800

    Update start_socat.sh

--- start_socat.sh
+++ start_socat.sh
@@ -1,6 +1,6 @@
-nohup socat TCP4-LISTEN:3333,reuseaddr,fork TCP4:47.112.175.21:3333 >> /root/socat.log 2>&1 &
-nohup socat UDP4-LISTEN:3333,reuseaddr,fork UDP4:47.112.175.21:3333 >> /root/socat.log 2>&1 &
-nohup socat TCP4-LISTEN:3334,reuseaddr,fork TCP4:47.112.175.21:3334 >> /root/socat.log 2>&1 &
-nohup socat UDP4-LISTEN:3334,reuseaddr,fork UDP4:47.112.175.21:3334 >> /root/socat.log 2>&1 &
-nohup socat TCP4-LISTEN:4333,reuseaddr,fork TCP4:47.112.175.21:4333 >> /root/socat.log 2>&1 &
-nohup socat UDP4-LISTEN:4333,reuseaddr,fork UDP4:47.112.175.21:4333 >> /root/socat.log 2>&1 &
+echo "nohup socat TCP4-LISTEN:3333,reuseaddr,fork TCP4:47.112.175.21:3333 >> /root/socat.log 2>&1 &"
+echo "nohup socat UDP4-LISTEN:3333,reuseaddr,fork UDP4:47.112.175.21:3333 >> /root/socat.log 2>&1 &"
+echo "nohup socat TCP4-LISTEN:3334,reuseaddr,fork TCP4:47.112.175.21:3334 >> /root/socat.log 2>&1 &"
+echo "nohup socat UDP4-LISTEN:3334,reuseaddr,fork UDP4:47.112.175.21:3334 >> /root/socat.log 2>&1 &"
+echo "nohup socat TCP4-LISTEN:4333,reuseaddr,fork TCP4:47.112.175.21:4333 >> /root/socat.log 2>&1 &"
+echo" nohup socat UDP4-LISTEN:4333,reuseaddr,fork UDP4:47.112.175.21:4333 >> /root/socat.log 2>&1 &"
