3af5ed7636fe1f8ed4e69663de7b5b3075ffaa35
wongjai <40750736+wongjai@users.noreply.github.com>
2020-08-14 17:45:46 +0800

    Update start_socat.sh

--- start_socat.sh
+++ start_socat.sh
@@ -3,4 +3,4 @@ echo "nohup socat UDP4-LISTEN:3333,reuseaddr,fork UDP4:47.112.175.21:3333 >> /ro
 echo "nohup socat TCP4-LISTEN:3334,reuseaddr,fork TCP4:47.112.175.21:3334 >> /root/socat.log 2>&1 &"
 echo "nohup socat UDP4-LISTEN:3334,reuseaddr,fork UDP4:47.112.175.21:3334 >> /root/socat.log 2>&1 &"
 echo "nohup socat TCP4-LISTEN:4333,reuseaddr,fork TCP4:47.112.175.21:4333 >> /root/socat.log 2>&1 &"
-echo" nohup socat UDP4-LISTEN:4333,reuseaddr,fork UDP4:47.112.175.21:4333 >> /root/socat.log 2>&1 &"
+echo "nohup socat UDP4-LISTEN:4333,reuseaddr,fork UDP4:47.112.175.21:4333 >> /root/socat.log 2>&1 &"
